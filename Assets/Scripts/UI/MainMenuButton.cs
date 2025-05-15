a7a15394395bc913ed5e0bc446276d3a0e0a25d4
Klainis <136218846+Klainis@users.noreply.github.com>
2025-05-15 23:56:10 +0500

    Начато создание интерфейса. Создано начальное представление main menu

---
+++ Assets/Scripts/UI/MainMenuButton.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButton : MonoBehaviour
+{
+    public void NewGame()
+    {
+        SceneManager.LoadScene("Game1");
+    }
+
+    public void LoadGame()
+    {
+        // Реализация загрузки (используйте PlayerPrefs или систему сохранений)
+    }
+
+    public void OpenSettings()
+    {
+        // Активируйте Panel с настройками (см. ниже)
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
